adb5adec8c3c191048db7417e2dae7bef0fa24a7
Manoj Waikar <mmwaikar@gmail.com>
2025-03-04 19:23:00 +0100

    Add tracker example.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Development
+
+## Branches
+
+`feature/tracker` for using trackers
+`feature/factories` for using factories
--- src/main.rs
+++ src/main.rs
@@ -1,207 +1,128 @@
-use gtk::glib::clone;
-use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt};
+use gtk::prelude::{BoxExt, ButtonExt, OrientableExt};
+use rand::prelude::IteratorRandom;
 use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 
-const NEWLINE: char = '\x0A';
-const BACKSPACE: char = '\x08';
+const ICON_LIST: &[&str] = &[
+    "bookmark-new-symbolic",
+    "edit-copy-symbolic",
+    "edit-cut-symbolic",
+    "edit-find-symbolic",
+    "starred-symbolic",
+    "system-run-symbolic",
+    "emoji-objects-symbolic",
+    "emoji-nature-symbolic",
+    "display-brightness-symbolic",
+];
 
-#[derive(Clone, Copy, Default, PartialEq)]
-pub enum AlphabetStatus {
-    #[default]
-    None,
-    Absent,
-    IncorrectPosition,
-    CorrectPosition,
+fn random_icon_name() -> &'static str {
+    ICON_LIST
+        .iter()
+        .choose(&mut rand::rng())
+        .expect("Could not choose a random icon")
 }
 
-#[derive(Clone, Copy, Default, PartialEq)]
-pub struct Key {
-    pub key: char,
-    pub row: usize,
-    pub status: AlphabetStatus,
-}
-
-impl Key {
-    fn new(key: char, row: usize) -> Key {
-        Key {
-            key,
-            row,
-            status: AlphabetStatus::None,
-        }
-    }
-
-    fn get_str(&self) -> String {
-        match self.key {
-            NEWLINE => "Enter".to_string(),
-            BACKSPACE => "<-".to_string(),
-            _ => self.key.to_string(),
-        }
-    }
-}
-
-#[derive(Clone, Default, PartialEq)]
-struct KeyboardModel {
-    pub first_row: Vec<Key>,
-    pub second_row: Vec<Key>,
-    pub third_row: Vec<Key>,
-}
-
-impl KeyboardModel {
-    fn init() -> KeyboardModel {
-
-        const FIRST_ROW: &str = "qwertyuiop";
-        const SECOND_ROW: &str = "asdfghjkl";
-        const THIRD_ROW: &str = "zxcvbnm";
-
-        let mut tr_chars = THIRD_ROW.chars().collect::<Vec<char>>();
-        tr_chars.push(BACKSPACE);
-        tr_chars.insert(0, NEWLINE);
-        dbg!(&tr_chars);
-
-        let first_row: Vec<Key> = FIRST_ROW.chars().map(|c| Key::new(c, 1)).collect();
-        let second_row: Vec<Key> = SECOND_ROW.chars().map(|c| Key::new(c, 2)).collect();
-        let third_row: Vec<Key> = tr_chars.into_iter().map(|c| Key::new(c, 3)).collect();
-
-        KeyboardModel {
-            first_row,
-            second_row,
-            third_row,
-        }
-    }
+// The track proc macro allows to easily track changes to different
+// fields of the model
+#[tracker::track]
+struct AppModel {
+    first_icon: &'static str,
+    second_icon: &'static str,
+    identical: bool,
 }
 
 #[derive(Debug)]
-pub enum KeyboardMsg {
-    KeyPressed(char),
-    // Enter,
-    // Backspace,
+enum AppInput {
+    UpdateFirst,
+    UpdateSecond,
 }
 
-struct AppWidgets {
-    label: gtk::Label,
-}
-
-impl SimpleComponent for KeyboardModel {
-    /// The type of the messages that this component can receive.
-    type Input = KeyboardMsg;
-    /// The type of the messages that this component can send.
+#[relm4::component]
+impl SimpleComponent for AppModel {
+    type Init = ();
+    type Input = AppInput;
     type Output = ();
-    /// The type of data with which this component will be initialized.
-    type Init = KeyboardModel;
-    /// The root GTK widget that this component will create.
-    type Root = gtk::Window;
-    /// A data structure that contains the widgets that you will need to update.
-    type Widgets = AppWidgets;
 
-    fn init_root() -> Self::Root {
-        gtk::Window::builder()
-            .title("Wordle")
-            .default_width(300)
-            .default_height(500)
-            .build()
+    view! {
+        #[root]
+        gtk::ApplicationWindow {
+            #[track = "model.changed(AppModel::identical())"]
+            set_class_active: ("identical", model.identical),
+            
+            gtk::Box {
+                set_orientation: gtk::Orientation::Horizontal,
+                set_spacing: 10,
+                set_margin_all: 10,
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 10,
+                    gtk::Image {
+                        set_pixel_size: 50,
+                        #[track = "model.changed(AppModel::first_icon())"]
+                        set_icon_name: Some(model.first_icon),
+                    },
+                    gtk::Button {
+                        set_label: "New random image",
+                        connect_clicked[sender] => move |_| {
+                            sender.input(AppInput::UpdateFirst)
+                        }
+                    }
+                },
+                append = &gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 10,
+                    gtk::Image {
+                        set_pixel_size: 50,
+                        #[track = "model.changed(AppModel::second_icon())"]
+                        set_icon_name: Some(model.second_icon),
+                    },
+                    gtk::Button {
+                        set_label: "New random image",
+                        connect_clicked[sender] => move |_| {
+                            sender.input(AppInput::UpdateSecond)
+                        }
+                    }
+                },
+            }
+        }
     }
 
-    /// Initialize the UI and model.
+    // Initialize the UI.
     fn init(
-        kb_model: Self::Init,
-        window: Self::Root,
+        _params: Self::Init,
+        root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = KeyboardModel::init();
+        let model = AppModel {
+            first_icon: random_icon_name(),
+            second_icon: random_icon_name(),
+            identical: false,
+            tracker: 0,
+        };
 
-        let label = gtk::Label::new(Some(&format!("Counter:")));
-        label.set_margin_all(5);
-
-        let vbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Vertical)
-            .spacing(5)
-            .build();
-
-        let f_hbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        f_hbox.set_margin_all(5);
-        f_hbox.set_align(gtk::Align::Center);
-
-        for &key in &model.first_row {
-            let button = gtk::Button::with_label(&key.get_str());
-            button.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(KeyboardMsg::KeyPressed(' '));
-                }
-            ));
-
-            f_hbox.append(&button);
-        }
-
-        let s_hbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        s_hbox.set_margin_all(5);
-        s_hbox.set_align(gtk::Align::Center);
-
-        for &key in &model.second_row {
-            let button = gtk::Button::with_label(&key.get_str());
-            button.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(KeyboardMsg::KeyPressed(' '));
-                }
-            ));
-
-            s_hbox.append(&button);
-        }
-
-        let t_hbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        t_hbox.set_margin_all(5);
-        t_hbox.set_align(gtk::Align::Center);
-
-        for &key in &model.third_row {
-            let button = gtk::Button::with_label(&key.get_str());
-            button.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(KeyboardMsg::KeyPressed(' '));
-                }
-            ));
-
-            t_hbox.append(&button);
-        }
-
-        window.set_child(Some(&vbox));
-        vbox.set_margin_all(5);
-        vbox.set_align(gtk::Align::Center);
-        vbox.append(&f_hbox);
-        vbox.append(&s_hbox);
-        vbox.append(&t_hbox);
-
-        let widgets = AppWidgets { label };
+        // Insert the macro code generation here
+        let widgets = view_output!();
 
         ComponentParts { model, widgets }
     }
 
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
+        // reset tracker value of the model
+        self.reset();
+
         match message {
-            KeyboardMsg::KeyPressed(c) => {
-                println!("key pressed: {}", c);
-                // self.counter = self.counter.wrapping_add(1);
-            } // KeyboardMsg::Decrement => {
-              //     self.counter = self.counter.wrapping_sub(1);
-              // }
+            AppInput::UpdateFirst => {
+                self.set_first_icon(random_icon_name());
+            }
+            AppInput::UpdateSecond => {
+                self.set_second_icon(random_icon_name());
+            }
         }
+        self.set_identical(self.first_icon == self.second_icon);
     }
 }
 
 fn main() {
     let app = RelmApp::new("relm4.test.simple");
-    app.run::<KeyboardModel>(KeyboardModel::init());
+    relm4::set_global_css(".identical { background: #00ad5c; }");
+    app.run::<AppModel>(());
 }
