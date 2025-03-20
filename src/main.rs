645dc5a1dc4f3a5a50bdd25f58527b4007be6360
Manoj Waikar <mmwaikar@gmail.com>
2025-03-20 15:41:12 +0100

    First working version of a button component.

--- src/main.rs
+++ src/main.rs
@@ -1,207 +1,195 @@
-use gtk::glib::clone;
-use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt};
-use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
+mod keyboard;
+
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+use relm4::{gtk, ComponentParts, ComponentSender, Controller, RelmApp, RelmWidgetExt, SimpleComponent};
+use relm4::*;
 
 const NEWLINE: char = '\x0A';
 const BACKSPACE: char = '\x08';
 
-#[derive(Clone, Copy, Default, PartialEq)]
-pub enum AlphabetStatus {
-    #[default]
-    None,
-    Absent,
-    IncorrectPosition,
-    CorrectPosition,
+#[tracker::track]
+struct Guess {
+    word: String,
 }
 
-#[derive(Clone, Copy, Default, PartialEq)]
-pub struct Key {
-    pub key: char,
-    pub row: usize,
-    pub status: AlphabetStatus,
+// #[derive(Clone, Copy, Debug, Default, PartialEq)]
+// enum ButtonStatus {
+//     #[default]
+//     None,
+//     Absent,
+//     IncorrectPosition,
+//     CorrectPosition,
+// }
+
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+struct ButtonModel {
+    text: char,
+    // row: usize,
+    // status: ButtonStatus,
 }
 
-impl Key {
-    fn new(key: char, row: usize) -> Key {
-        Key {
-            key,
-            row,
-            status: AlphabetStatus::None,
+impl ButtonModel {
+    pub fn new(text: char) -> ButtonModel {
+        ButtonModel {
+            text,
+            // row,
+            // status: ButtonStatus::None,
         }
     }
 
-    fn get_str(&self) -> String {
-        match self.key {
+    pub fn get_str(&self) -> String {
+        match self.text {
             NEWLINE => "Enter".to_string(),
             BACKSPACE => "<-".to_string(),
-            _ => self.key.to_string(),
-        }
-    }
-}
-
-#[derive(Clone, Default, PartialEq)]
-struct KeyboardModel {
-    pub first_row: Vec<Key>,
-    pub second_row: Vec<Key>,
-    pub third_row: Vec<Key>,
-}
-
-impl KeyboardModel {
-    fn init() -> KeyboardModel {
-
-        const FIRST_ROW: &str = "qwertyuiop";
-        const SECOND_ROW: &str = "asdfghjkl";
-        const THIRD_ROW: &str = "zxcvbnm";
-
-        let mut tr_chars = THIRD_ROW.chars().collect::<Vec<char>>();
-        tr_chars.push(BACKSPACE);
-        tr_chars.insert(0, NEWLINE);
-        dbg!(&tr_chars);
-
-        let first_row: Vec<Key> = FIRST_ROW.chars().map(|c| Key::new(c, 1)).collect();
-        let second_row: Vec<Key> = SECOND_ROW.chars().map(|c| Key::new(c, 2)).collect();
-        let third_row: Vec<Key> = tr_chars.into_iter().map(|c| Key::new(c, 3)).collect();
-
-        KeyboardModel {
-            first_row,
-            second_row,
-            third_row,
+            _ => self.text.to_string(),
         }
     }
 }
 
 #[derive(Debug)]
-pub enum KeyboardMsg {
-    KeyPressed(char),
+pub enum ButtonPressedMsg {
+    ButtonPressed,
     // Enter,
     // Backspace,
 }
 
-struct AppWidgets {
-    label: gtk::Label,
+#[derive(Debug)]
+pub struct CharInput {
+    pub text: char,
 }
 
-impl SimpleComponent for KeyboardModel {
-    /// The type of the messages that this component can receive.
-    type Input = KeyboardMsg;
-    /// The type of the messages that this component can send.
-    type Output = ();
+#[relm4::component]
+impl SimpleComponent for ButtonModel {
     /// The type of data with which this component will be initialized.
-    type Init = KeyboardModel;
-    /// The root GTK widget that this component will create.
-    type Root = gtk::Window;
-    /// A data structure that contains the widgets that you will need to update.
-    type Widgets = AppWidgets;
+    type Init = char;
+    /// The type of the messages that this component can receive.
+    type Input = ButtonPressedMsg;
+    /// The type of the messages that this component can send.
+    type Output = CharInput;
 
-    fn init_root() -> Self::Root {
-        gtk::Window::builder()
-            .title("Wordle")
-            .default_width(300)
-            .default_height(500)
-            .build()
+    view! {
+        #[root]
+        gtk::Button {
+            set_label: model.text.to_string().as_str(),
+            connect_clicked => ButtonPressedMsg::ButtonPressed
+        }
     }
 
-    /// Initialize the UI and model.
+    // Initialize the UI.
     fn init(
-        kb_model: Self::Init,
-        window: Self::Root,
+        button_text: Self::Init,
+        root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = KeyboardModel::init();
-
-        let label = gtk::Label::new(Some(&format!("Counter:")));
-        label.set_margin_all(5);
-
-        let vbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Vertical)
-            .spacing(5)
-            .build();
-
-        let f_hbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        f_hbox.set_margin_all(5);
-        f_hbox.set_align(gtk::Align::Center);
-
-        for &key in &model.first_row {
-            let button = gtk::Button::with_label(&key.get_str());
-            button.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(KeyboardMsg::KeyPressed(' '));
-                }
-            ));
-
-            f_hbox.append(&button);
-        }
-
-        let s_hbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        s_hbox.set_margin_all(5);
-        s_hbox.set_align(gtk::Align::Center);
-
-        for &key in &model.second_row {
-            let button = gtk::Button::with_label(&key.get_str());
-            button.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(KeyboardMsg::KeyPressed(' '));
-                }
-            ));
-
-            s_hbox.append(&button);
-        }
-
-        let t_hbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(5)
-            .build();
-        t_hbox.set_margin_all(5);
-        t_hbox.set_align(gtk::Align::Center);
-
-        for &key in &model.third_row {
-            let button = gtk::Button::with_label(&key.get_str());
-            button.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(KeyboardMsg::KeyPressed(' '));
-                }
-            ));
-
-            t_hbox.append(&button);
-        }
-
-        window.set_child(Some(&vbox));
-        vbox.set_margin_all(5);
-        vbox.set_align(gtk::Align::Center);
-        vbox.append(&f_hbox);
-        vbox.append(&s_hbox);
-        vbox.append(&t_hbox);
-
-        let widgets = AppWidgets { label };
-
+        let model = ButtonModel { text: button_text };
+        let widgets = view_output!();
         ComponentParts { model, widgets }
     }
 
-    fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
-        match message {
-            KeyboardMsg::KeyPressed(c) => {
-                println!("key pressed: {}", c);
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            ButtonPressedMsg::ButtonPressed => {
+                println!("key pressed: {}", self.text);
                 // self.counter = self.counter.wrapping_add(1);
-            } // KeyboardMsg::Decrement => {
-              //     self.counter = self.counter.wrapping_sub(1);
-              // }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+enum AppMode {
+    View,
+    Edit,
+    Export,
+}
+
+struct AppModel {
+    counter: u8,
+    button: Controller<ButtonModel>,
+}
+
+#[derive(Debug)]
+enum AppMsg {
+    SetMode(AppMode),
+    Increment,
+    Decrement,
+}
+
+#[relm4::component]
+impl SimpleComponent for AppModel {
+    type Init = u8;
+
+    type Input = AppMsg;
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            set_title: Some("Simple app"),
+            set_default_width: 300,
+            set_default_height: 100,
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 5,
+                set_margin_all: 5,
+
+                gtk::Button {
+                    set_label: "Increment",
+                    connect_clicked => AppMsg::Increment
+                },
+
+                gtk::Button::with_label("Decrement") {
+                    connect_clicked => AppMsg::Decrement
+                },
+
+                gtk::Label {
+                    #[watch]
+                    set_label: &format!("Counter: {}", model.counter),
+                    set_margin_all: 5,
+                },
+
+                model.button.widget(),
+            }
+        }
+    }
+
+    // Initialize the UI.
+    fn init(
+        counter: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let button: Controller<ButtonModel> =
+            ButtonModel::builder()
+                .launch('w')
+                .forward(sender.input_sender(), |msg| {
+                    println!("key pressed: {}", msg.text);
+                    AppMsg::SetMode(AppMode::View)
+                });
+
+        let model = AppModel { counter, button };
+
+        // Insert the macro code generation here
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            AppMsg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            AppMsg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
+            AppMsg::SetMode(m) => {
+                println!("set mode: {:?}", m);
+            }
         }
     }
 }
 
 fn main() {
     let app = RelmApp::new("relm4.test.simple");
-    app.run::<KeyboardModel>(KeyboardModel::init());
+    app.run::<AppModel>(0);
 }
